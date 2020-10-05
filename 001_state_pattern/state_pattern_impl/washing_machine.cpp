91d366deb2c6eb82eaf01f671c325eb43e7a1ece
Pradeepa Senanayake <pradeepa.senanayake@resmed.com.au>
2020-10-05 15:09:00 +1100

    Cleaning up

--- 001_state_pattern/state_pattern_impl/washing_machine.cpp
+++ 001_state_pattern/state_pattern_impl/washing_machine.cpp
@@ -20,7 +20,8 @@ WashingMachine::WashingMachine(
         ILaundrySensor& laundrySensor, IIndicator& indicator,
         IUserInputs& userInputs, IWashingCycles& washingCycles, IWaterSensor& waterSensor):
     m_laundrySensor(laundrySensor), m_indicator(indicator), m_userInputs(userInputs),
-    m_washCycles(washingCycles), m_waterSensor(waterSensor)
+    m_washCycles(washingCycles), m_waterSensor(waterSensor), m_currentState {nullptr},
+    m_prevStateName {StateName::IDLE}, m_waterLevelTarget {IWaterSensor::WaterLevel::L2}
 {
     auto idleState = make_shared<IdleState>(*this, m_laundrySensor);
     auto standbyState = make_shared<StandbyState>(*this, m_laundrySensor, m_indicator, m_userInputs);
@@ -33,30 +34,30 @@ WashingMachine::WashingMachine(
 
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::IDLE, idleState));
+                StateName::IDLE, idleState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::STANDBY, standbyState));
+                StateName::STANDBY, standbyState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::ADD_WATER, addWaterState));
+                StateName::ADD_WATER, addWaterState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::WASH, washState));
+                StateName::WASH, washState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::RINSE, rinseState));
+                StateName::RINSE, rinseState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::SPIN, spinState));
+                StateName::SPIN, spinState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::DONE, doneState));
+                StateName::DONE, doneState));
     m_states.insert(
             make_pair(
-                IWashingMachineState::State::ERROR, errorState));
+                StateName::ERROR, errorState));
 
-    m_currentState = m_states[IWashingMachineState::State::IDLE];
+    m_currentState = m_states[StateName::IDLE];
 }
 
 void WashingMachine::Run()
@@ -65,159 +66,16 @@ void WashingMachine::Run()
     m_currentState->Run();
 }
 
-void WashingMachine::ChangeState(IWashingMachineState::State state)
+void WashingMachine::ChangeState(StateName state)
 {
     m_prevStateName = m_currentState->WhoAmI();
     m_currentState = m_states[state];
     m_currentState->Reset();
 }
 
-void WashingMachine::Run2()
-{
-    m_indicator.SetActualWaterLevel(Convert(m_waterSensor.GetLevel()));
-
-    if (m_state != WashingMachineState::ERROR && m_washCycles.IsInError())
-    {
-        m_preErrorState = m_state;
-        m_state = WashingMachineState::ERROR;
-    }
-
-    switch (m_state)
-    {
-        case WashingMachineState::IDLE:
-            if (m_laundrySensor.GetLevel() != ILaundrySensor::LaundryLevel::NONE)
-            {
-                m_state = WashingMachineState::STANDBY;
-            }
-            break;
-
-        case WashingMachineState::STANDBY:
-            m_indicator.SetLaundryLevel(Convert(m_laundrySensor.GetLevel()));
-            m_recommendedWaterLevel = GetRecommendedWaterLevel(m_laundrySensor.GetLevel());
-            m_indicator.SetRecommendedWaterLevel(Convert(m_recommendedWaterLevel));
-
-            if (m_userInputs.HasStartButtonPressed())
-            {
-                m_state = WashingMachineState::STARTING_WATER;
-            }
-            break;
-
-        case WashingMachineState::STARTING_WATER:
-            m_washCycles.StartWater();
-            m_state = WashingMachineState::ADD_WATER;
-            break;
-
-        case WashingMachineState::ADD_WATER:
-            if (RecommendedWaterLevelReached())
-            {
-                m_washCycles.StopWater();
-                m_state = WashingMachineState::STARTING_WASH;
-            }
-            break;
-
-        case WashingMachineState::STARTING_WASH:
-            m_washCycles.StartWashAlgorithm();
-            m_indicator.SetState(IIndicator::MachineState::WASHING);
-            m_state = WashingMachineState::WASH;
-            break;
-
-        case WashingMachineState::WASH:
-            if (m_washCycles.IsAlgorithmDone())
-            {
-                m_state = WashingMachineState::STARTING_RINSE;
-            }
-            break;
-
-        case WashingMachineState::STARTING_RINSE:
-            m_washCycles.StartRinseAlgorithm();
-            m_indicator.SetState(IIndicator::MachineState::RINSE);
-            m_state = WashingMachineState::RINSE;
-            break;
-
-        case WashingMachineState::RINSE:
-            if (m_washCycles.IsAlgorithmDone())
-            {
-                m_state = WashingMachineState::STARTING_SPIN;
-            }
-            break;
-
-        case WashingMachineState::STARTING_SPIN:
-            m_washCycles.StartSpinAlgorithm();
-            m_indicator.SetState(IIndicator::MachineState::SPIN);
-            m_state = WashingMachineState::SPIN;
-            break;
-
-        case WashingMachineState::SPIN:
-            if (m_washCycles.IsAlgorithmDone())
-            {
-                m_washCycles.StopSpinAlgorithm();
-                m_indicator.SetState(IIndicator::MachineState::DONE);
-                m_state = WashingMachineState::DONE;
-            }
-            break;
-
-        case WashingMachineState::DONE:
-            if (m_laundrySensor.GetLevel() == ILaundrySensor::LaundryLevel::NONE)
-            {
-                m_indicator.SetLaundryLevel(Convert(m_laundrySensor.GetLevel()));
-                m_indicator.SetRecommendedWaterLevel(IIndicator::WaterLevel::NONE);
-                m_state = WashingMachineState::IDLE;
-            }
-            break;
-
-        case WashingMachineState::ERROR:
-            m_indicator.SetState(IIndicator::MachineState::ERROR);
-            if (m_userInputs.HasStartButtonPressed())
-            {
-                m_washCycles.ClearError();
-                m_state = GetPreErrorState(m_preErrorState);
-            }
-            else if (m_userInputs.HasStopButtonPressed())
-            {
-                m_washCycles.Reset();
-                m_state = WashingMachineState::IDLE;
-                m_indicator.SetState(IIndicator::MachineState::IDLE);
-            }
-            break;
-
-        default:
-            break;
-    }
-}
-
 IWashingMachineState::State WashingMachine::GetPreviousState() const
 {
     return m_prevStateName;
-}
-
-IIndicator::LaundryLevel WashingMachine::Convert(ILaundrySensor::LaundryLevel level)
-{
-    switch (level)
-    {
-        case ILaundrySensor::LaundryLevel::NONE:
-            return IIndicator::LaundryLevel::NONE;
-        case ILaundrySensor::LaundryLevel::L1:
-            return IIndicator::LaundryLevel::L1;
-        case ILaundrySensor::LaundryLevel::L2:
-            return IIndicator::LaundryLevel::L2;
-    }
-
-    return IIndicator::LaundryLevel::NONE;
-}
-
-IIndicator::WaterLevel WashingMachine::Convert(IWaterSensor::WaterLevel level)
-{
-    switch (level)
-    {
-        case IWaterSensor::WaterLevel::NONE:
-            return IIndicator::WaterLevel::NONE;
-        case IWaterSensor::WaterLevel::L1:
-            return IIndicator::WaterLevel::L1;
-        case IWaterSensor::WaterLevel::L2:
-            return IIndicator::WaterLevel::L2;
-    }
-
-    return IIndicator::WaterLevel::NONE;
 }
 
 void WashingMachine::SetWaterLevelTarget(IWaterSensor::WaterLevel level)
@@ -243,26 +101,4 @@ IWaterSensor::WaterLevel WashingMachine::GetRecommendedWaterLevel(ILaundrySensor
     }
 
     return IWaterSensor::WaterLevel::NONE;
-}
-
-bool WashingMachine::RecommendedWaterLevelReached()
-{
-    return m_recommendedWaterLevel == m_waterSensor.GetLevel();
-}
-
-WashingMachine::WashingMachineState WashingMachine::GetPreErrorState(WashingMachineState state)
-{
-    switch (state)
-    {
-        case WashingMachineState::ADD_WATER:
-            return WashingMachineState::STARTING_WATER;
-        case WashingMachineState::WASH:
-            return WashingMachineState::STARTING_WASH;
-        case WashingMachineState::RINSE:
-            return WashingMachineState::STARTING_RINSE;
-        case WashingMachineState::SPIN:
-            return WashingMachineState::STARTING_SPIN;
-        default:
-            return state;
-    }
 }
--- 001_state_pattern/state_pattern_impl/washing_machine.hpp
+++ 001_state_pattern/state_pattern_impl/washing_machine.hpp
@@ -14,22 +14,10 @@ class IWashingMachineState;
 
 class WashingMachine: public IWashingMachineContext
 {
-public:
-    enum class WashingMachineState
-    {
-        IDLE,
-        STANDBY,
-        STARTING_WATER,
-        ADD_WATER,
-        STARTING_WASH,
-        WASH,
-        STARTING_RINSE,
-        RINSE,
-        STARTING_SPIN,
-        SPIN,
-        DONE,
-        ERROR
-    };
+private:
+    using State = std::shared_ptr<IWashingMachineState>;
+    using StateName = IWashingMachineState::State;
+    using StateMap = std::map<StateName, State>;
 
 public:
     WashingMachine(
@@ -37,36 +25,24 @@ public:
             IWashingCycles& washingCycles, IWaterSensor& waterSensor);
 
     void Run();
-    void Run2();
-    void ChangeState(IWashingMachineState::State state) override;
+
+    // IWashingMachineContext interface
+    void ChangeState(StateName state) override;
     IWaterSensor::WaterLevel GetRecommendedWaterLevel(ILaundrySensor::LaundryLevel level) override;
     void SetWaterLevelTarget(IWaterSensor::WaterLevel level) override;
     IWaterSensor::WaterLevel GetWaterLevelTarget() const override;
-    IWashingMachineState::State GetPreviousState() const override;
+    StateName GetPreviousState() const override;
 
 private:
-    using State = std::shared_ptr<IWashingMachineState>;
-
-    IIndicator::LaundryLevel Convert(ILaundrySensor::LaundryLevel level);
-    IIndicator::WaterLevel Convert(IWaterSensor::WaterLevel level);
-    WashingMachineState GetPreErrorState(WashingMachineState state);
-
-    WashingMachineState m_state {WashingMachineState::IDLE};
-    WashingMachineState m_preErrorState {WashingMachineState::IDLE};
-    IWashingMachineState::State m_prevStateName {};
-    IWaterSensor::WaterLevel m_recommendedWaterLevel {IWaterSensor::WaterLevel::L2};
-    IWaterSensor::WaterLevel m_waterLevelTarget {IWaterSensor::WaterLevel::L2};
-
     ILaundrySensor& m_laundrySensor;
     IIndicator& m_indicator;
     IUserInputs& m_userInputs;
     IWashingCycles& m_washCycles;
     IWaterSensor& m_waterSensor;
-
-    State m_currentState {nullptr};
-
-    bool RecommendedWaterLevelReached();
-    std::map<IWashingMachineState::State, State> m_states;
+    State m_currentState;
+    StateName m_prevStateName;
+    IWaterSensor::WaterLevel m_waterLevelTarget;
+    StateMap m_states;
 };
 
 #endif  // _WASHING_MACHINE_HPP
